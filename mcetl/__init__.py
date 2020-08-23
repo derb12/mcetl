ad13be9cf17ea9d953d5f4ceb93af2e37e9d4620
derb12 <55961724+derb12@users.noreply.github.com>
2020-08-23 11:59:40 -0400

    More changes from the initial setup.
    Added actual requirements for the project.  Minor changes to the autogenerated files.

--- mcetl/__init__.py
+++ mcetl/__init__.py
@@ -9,9 +9,13 @@ Created on Wed Jul 15 23:48:02 2020
 
 """
 
-__author__ = """Donald Erb"""
-__version__ = '0.1.0'
 
-
+from .datasource import DataSource
+from .functions import SeparationFunction, CalculationFunction, SummaryFunction
 from .generate_raw_data import generate_raw_data
 from .main_gui import launch_main_gui
+#include fitting and plotting convenience functions
+
+
+__author__ = """Donald Erb"""
+__version__ = '0.1.0'
