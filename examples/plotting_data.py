a1afcd92e63354085ec5a1b04f6cab64abca7434
derb12 <55961724+derb12@users.noreply.github.com>
2020-08-24 23:32:01 -0400

    Made more example programs
    Made example programs to use plotting_gui and to use main_gui.

---
+++ examples/plotting_data.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Shows how to use plotting_gui from mcetl.
+
+@author: Donald Erb
+Created on Sat Aug 22 17:34:39 2020
+
+"""
+
+
+import PySimpleGUI as sg
+from mcetl.plotting_gui import configure_plots
+import mcetl.utils as utils
+
+
+#changes some defaults for the plot formatting
+changes = {
+    'font.serif': 'Times New Roman',
+    'font.family': 'serif',
+    'font.size': 12,
+    'mathtext.default': 'regular',
+    'xtick.direction': 'in',
+    'ytick.direction': 'in',
+    'xtick.minor.visible': True,
+    'ytick.minor.visible': True,
+    'xtick.major.size': 5,
+    'xtick.major.width': 0.6,
+    'xtick.minor.size': 2.5,
+    'xtick.minor.width': 0.6,
+    'ytick.major.size': 5,
+    'ytick.major.width': 0.6,
+    'ytick.minor.size': 2.5,
+    'ytick.minor.width': 0.6,
+    'lines.linewidth': 2,
+    'lines.markersize': 5,
+    'axes.linewidth': 0.6,
+    'legend.frameon': False
+}
+
+try:
+    num_files = sg.popup_get_text('Enter number of files to open', 'Get Files', '1')
+    if num_files:
+        dataframes = []
+        for _ in range(int(num_files)):
+        #gets the values needed to import a datafile, and then imports the data to a dataframe
+            import_values = utils.select_file_gui()
+            dataframes.append(
+                utils.raw_data_import(import_values, import_values['file'], False)
+            )
+        figures = configure_plots(dataframes, changes)
+
+except utils.WindowCloseError:
+    pass
+except KeyboardInterrupt:
+    pass
+
+#figures = load_previous_figure(new_rc_changes={'font.size': 12})
---
+++ examples/use_main_gui.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""Example script to show how to use mcetl.launch_main_gui with defined DataSource objects.
+
+@author: Donald Erb
+Created on Sat Aug 22 13:01:37 2020
+
+"""
+
+import numpy as np
+import mcetl
+from mcetl import DataSource, CalculationFunction
+
+
+def offset_func(df, target_indices, calc_indices, excel_columns=None, start_row=0, offset=None):
+    """Example CalculationFunction with named kwargs"""
+
+    for i, sample in enumerate(calc_indices):
+        for j, calc_col in enumerate(sample):
+            if excel_columns is not None:
+                x = df[target_indices[0][i][j]]
+                x_col = excel_columns[target_indices[0][i][j]]
+                y_col = excel_columns[target_indices[1][i][j]]
+                calc = [
+                    f'= {x_col}{k+3+start_row} + {y_col}{k+3+start_row} + {offset}' for k in range(len(x))
+                ]
+
+                df[calc_col] = np.where(~np.isnan(x), calc, '')
+            else:
+                x_col = df[df.columns[target_indices[0][i][j]]]
+                y_col = df[df.columns[target_indices[1][i][j]]]
+
+                df[df.columns[calc_col]] = x_col + y_col + offset
+
+        offset += offset
+
+    return df
+
+
+if __name__ == '__main__':
+
+    offset = CalculationFunction('offset', 'Intensity', offset_func, 1, {'offset: 1000'})
+    
+    #Definitions for each data source
+    xrd = DataSource(
+        name='XRD',
+        column_names=['2\u03B8 (\u00B0)', 'Intensity (Counts)', 'Offset Intensity (a.u.)'],
+        functions=offset, column_numbers=[1, 2],
+        start_row=1, end_row=0, separator=',',
+        xy_plot_indices=[0, 2], file_type='csv', num_files=1,
+        unique_variables=['2\u03B8', 'Intensity']
+    )
+    
+    """
+    ftir = DataSource('FTIR', ['Wavenumber (1/cm)','Absorbance (a.u.)',
+                                     'Offset Absorbance (a.u.)'],
+                                     ['offset', 'normalize', 'fit_peaks'], [0, 1],
+                                     1, 0, 0, ',', [0, 1], [0, 2], 'csv')
+    raman = DataSource('Raman', ['Raman Shift (1/cm)','Intensity (a.u.)',
+                                      'Offset Intensity (a.u.)'],
+                                      ['offset', 'normalize', 'fit_peaks'], [0, 1],
+                                      1, 0, 0, '\\t', [0, 1], [0, 2], 'txt')
+    tga = DataSource('TGA', ['Temperature (\u00B0C)', 'Time (min)',
+                                    'Mass (%)', 'Mass Loss Rate (%/\u00B0C)',
+                                    'Fraction Mass Lost'],
+                                    ['negative_derivative', 'max_x', 'fit_peaks',
+                                     'fractional_change'],
+                                    [0, 1, 2], None, 34, 0, ';', [0, 2], [0, 2], 'txt')
+    """
+    
+    other = DataSource('Other')
+    
+    #Put all DataSource objects in this tuple in order to use them
+    data_sources = (xrd, other)
+    
+    #call the main function with data_sources as the input
+    dataframes, proc_dataframes, fit_results = mcetl.launch_main_gui(data_sources)
