e0a4383c551d23d6bf2388a4b0e31d3ebbfd899c
derb12 <55961724+derb12@users.noreply.github.com>
2020-10-05 13:25:19 -0400

    Bump version v0.1.2 -> v0.2.0
    Former-commit-id: bf44fb4c954f6e75efcba6b9a547ec05583910a0

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/derb12/mcetl',
-    version='0.1.2',
+    version='0.2.0',
     zip_safe=False,
 )
