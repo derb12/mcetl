51e241af27a5fbc1e165ad48f76487388d625778
derb12 <55961724+derb12@users.noreply.github.com>
2020-10-30 18:26:31 -0400

    Update creating_functions.py
    Added example for when there is more than one column added by a function.

--- examples/creating_functions.py
+++ examples/creating_functions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-"""This is an example program to show how to create DataSource and Functions for mcetl.
+"""This is a program that shows the internals of what occurs during function processing for DataSources.
 
+The steps for processing are more clearly broken down so that each step can be understood.
 
 
 @author: Donald Erb
@@ -71,6 +72,32 @@ def func(df, target_indices, calc_indices, excel_columns=None, start_row=0, offs
     """Example CalculationFunction with named kwargs"""
 
     for i, sample in enumerate(calc_indices):
+        for j in range(len(sample) // 2): # 2 calc columns per entry in each sample
+            calc_col = sample[j * 2]
+            if excel_columns is not None:
+                x = df[target_indices[0][i][j]]
+                x_col = excel_columns[target_indices[0][i][j]]
+                y_col = excel_columns[target_indices[1][i][j]]
+                calc = [
+                    f'= {x_col}{k + start_row} + {y_col}{k + start_row} + {offset}' for k in range(len(x))
+                ]
+
+                df[calc_col] = np.where(~np.isnan(x), calc, '')
+            else:
+                x_col = df[df.columns[target_indices[0][i][j]]]
+                y_col = df[df.columns[target_indices[1][i][j]]]
+
+                df[df.columns[calc_col]] = x_col + y_col + offset
+
+        offset += offset
+
+    return df
+
+
+def func2(df, target_indices, calc_indices, excel_columns=None, start_row=0, offset=None):
+    """Example CalculationFunction with named kwargs"""
+
+    for i, sample in enumerate(calc_indices):
         for j, calc_col in enumerate(sample):
             if excel_columns is not None:
                 x = df[target_indices[0][i][j]]
@@ -92,7 +119,7 @@ def func(df, target_indices, calc_indices, excel_columns=None, start_row=0, offs
     return df
 
 
-def func2(df, target_indices, calc_indices, excel_columns=None, *args, **kwargs):
+def func3(df, target_indices, calc_indices, excel_columns=None, *args, **kwargs):
     """Example CalculationFunction using *args and **kwargs to swallow additional keyword arguments."""
 
     for i, sample in enumerate(calc_indices):
@@ -113,15 +140,16 @@ def func2(df, target_indices, calc_indices, excel_columns=None, *args, **kwargs)
 
 
 #Functions
-calculation = CalculationFunction('calc', ['x', 'y'], func, 1, {'offset': 10})
-calculation2 = CalculationFunction('calc2', ['calc'], func2, 'calc', {'offset': 1})
+calculation = CalculationFunction('calc', ['x', 'y'], func, 2, {'offset': 10})
+calculation2 = CalculationFunction('calc2', ['x', 'y'], func2, 1, {'offset': 5})
+calculation3 = CalculationFunction('calc3', ['calc2'], func3, 'calc2', {'offset': 1})
 separator = SeparationFunction('sep', 'x', split, None)
 sum1 = SummaryFunction('sum', 'x', f, added_columns=2)
 sum3 = SummaryFunction('sum3', 'x', f2, sample_summary=False)
 
 #Data Source
 xrd = DataSource(
-    'xrd', functions=[calculation, calculation2, separator, sum1, sum3],
+    'xrd', functions=[calculation, calculation2, calculation3, separator, sum1, sum3],
     unique_variables=['x', 'y'], sample_separation=3, entry_separation=1,
     excel_row_offset=1, excel_column_offset=1
 )
@@ -140,7 +168,7 @@ dataframes = [
     [[pd.DataFrame(data)], [pd.DataFrame(data2)]]
 ]
 
-#emulates importing the data as done in mcetl.main_gui
+#emulates importing the data as done in mcetl.launch_main_gui
 import_vals = [[[] for sample in dataset] for dataset in dataframes]
 for i, dataset in enumerate(dataframes):
     for j, sample in enumerate(dataset):
@@ -148,26 +176,26 @@ for i, dataset in enumerate(dataframes):
             import_vals[i][j].append({'index_x': 1, 'index_y': 0})
             sample[k] = utils.optimize_memory(measurement)
 
-
 #perform separation calcs
 dataframes, import_vals = data_source.do_separation_functions(
     dataframes, import_vals
 )
 #assign reference indices for all relevant columns
 data_source.set_references(dataframes, import_vals)
+references = data_source.references
 
 #merge dfs for each dataset
 merged_dataframes = data_source.merge_datasets(dataframes)
 
 #to compare excel vs python formulas
-dfs2 = [df.copy() for df in merged_dataframes]
+merged_dataframes2 = [df.copy() for df in merged_dataframes]
 
 #doing excel formulas
 merged_dataframes = data_source.do_excel_functions(merged_dataframes)
 
 #doing python formulas
-dfs2 = data_source.do_python_functions(dfs2)
+merged_dataframes2 = data_source.do_python_functions(merged_dataframes2)
 
 #split data back into individual dataframes
 output_dfs = data_source.split_into_entries(merged_dataframes)
-output_dfs2 = data_source.split_into_entries(dfs2)
+output_dfs2 = data_source.split_into_entries(merged_dataframes2)
