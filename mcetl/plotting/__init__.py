4e2ab9a07b69ebbdd24e7d391769fd8b4c4d5229
derb12 <55961724+derb12@users.noreply.github.com>
2020-11-16 12:01:18 -0500

    FEAT: Reordered module layout
    Moved all fitting related files to a new "fitting" folder and moved all plotting related files to a new "plotting" folder. This will allow expansion of the fitting and plotting sections without burdening the main folder. Renamed peak_fitting_gui to fitting_gui since I intend to extend the fitting beyond just peak fitting.
    
    
    Former-commit-id: 2155d5ccf1486891237c09a2c6ca663c7f1f764d

---
+++ mcetl/plotting/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Provides all objects related to plotting data.
+
+
+@author: Donald Erb
+Created on Nov 15, 2020
+
+"""
+
+
+from .plotting_gui import launch_plotting_gui, load_previous_figure
