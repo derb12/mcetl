6098e234cb41843145396ad59929216efcf3f076
derb12 <55961724+derb12@users.noreply.github.com>
2020-11-25 23:55:12 -0500

    FEAT: Added BWF function for peak fitting
    Added a modified Breit-Wigner-Fano peak model for fitting. Fixed issue where Voigt models with manual peak selection and vary gamma parameter set to True would not set an initial value for gamma.
    
    
    Former-commit-id: ce562dca89c60066b00ba5a5f08b615649c1fad6

---
+++ mcetl/fitting/fitting_utils.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""Provides utility functions, classes, and constants for the fitting module.
+
+Useful functions are put here in order to prevent circular importing
+within the other files.
+
+@author  : Donald Erb
+Created on Nov 18, 2020
+
+"""
+
+
+import lmfit
+
+
+def alternate_BWF(x, height=1.0, center=0.0, sigma=1.0, q=5.0):
+    """
+    An alternate Breit-Wigner-Fano lineshape that uses height rather than amplitude.
+
+    alternate_BWF(x, height, center, width, q) =
+        height*(1 + (x - center) / (q * sigma))**2 / (1 + ((x - center) / sigma)**2)
+
+    """
+
+    return height * (1 + (x - center) / (q * sigma))**2 / (1 + ((x - center) / sigma)**2)
+
+
+class ModifiedBWF(lmfit.Model):
+    """
+    A modified version of lmfit's BreitWignerFano model.
+
+    Initializes with q=5 rather than q=1, which gives a better peak.
+    Also defines terms for fwhm, height, x_mode, and maximum that were
+    not included in lmfit's implementation, where x_mode is the x position
+    at the maximum y-value, and maximum is the maximum y-value.
+
+    """
+
+    def __init__(self, independent_vars=['x'], prefix='', nan_policy='raise',
+                 **kwargs):
+        kwargs.update({'prefix': prefix, 'nan_policy': nan_policy,
+                       'independent_vars': independent_vars})
+        super().__init__(alternate_BWF, **kwargs)
+        self._set_paramhints_prefix()
+
+
+    def _set_paramhints_prefix(self):
+        self.set_param_hint('sigma', min=0.0)
+        self.set_param_hint(
+            'fwhm',
+            expr=f'2 * {self.prefix}sigma * (1 + {self.prefix}q**2) / max({lmfit.models.tiny}, abs(-1 + {self.prefix}q**2))')
+        self.set_param_hint('x_mode', expr=f'UNDEFINED if {self.prefix}q == 0 else {self.prefix}center + {self.prefix}sigma/(2*{self.prefix}q)')
+        self.set_param_hint('maximum', expr=f'{self.prefix}height * (1 + 1 / max({lmfit.models.tiny}, {self.prefix}q**2))')
+
+
+    def guess(self, data, x=None, negative=False, **kwargs):
+        """
+        Estimate initial model parameter values from data.
+
+        Notes
+        -----
+        For most lmfit models, amplitude is the peak area; for lmfit's
+        BreitWigner model, however, aplitude is the baseline of the distribution
+        (the y-value as abs(x) approaches infinity), so need
+
+        """
+
+        pars = lmfit.models.guess_from_peak(self, data, x, negative)
+        pars[f'{self.prefix}q'].set(value=5.0)
+        pars[f'{self.prefix}height'].set(value=min(data) if negative else max(data))
+
+        return lmfit.models.update_param_vals(pars, self.prefix, **kwargs)
+
+
+class ModifiedLognormal(lmfit.models.LognormalModel):
+    """
+    """
+
+
+def peak_directory():
+    """
+    [summary]
+
+    """
+
+    {'BreitWignerModel': (lmfit.models.BreitWignerModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['q', 1.0, float]]), 'ComplexConstantModel': (lmfit.models.ComplexConstantModel, [['re', 0.0, float], ['im', 0.0, float]]), 'ConstantModel': (lmfit.models.ConstantModel, [['c', 0.0, float]]), 'DampedHarmonicOscillatorModel': (lmfit.models.DampedHarmonicOscillatorModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['gamma', 1.0, float]]),
+    'DampedOscillatorModel': (lmfit.models.DampedOscillatorModel, [['amplitude', 1.0, float], ['center', 1.0, float], ['sigma', 0.1, float]]), 'DoniachModel': (lmfit.models.DoniachModel, [['amplitude', 1.0, float], ['center', 0, int], ['sigma', 1.0, float], ['gamma', 0.0, float]]),
+    'ExponentialGaussianModel': (lmfit.models.ExponentialGaussianModel, [['amplitude', 1, int], ['center', 0, int], ['sigma', 1.0, float], ['gamma', 1.0, float]]), 'ExponentialModel': (lmfit.models.ExponentialModel, [['amplitude', 1, int], ['decay', 1, int]]),
+    'GaussianModel': (lmfit.models.GaussianModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float]]), 'LinearModel': (lmfit.models.LinearModel, [['slope', 1.0, float], ['intercept', 0.0, float]]),
+    'LognormalModel': (lmfit.models.LognormalModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1, int]]), 'LorentzianModel': (lmfit.models.LorentzianModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float]]),
+    'MoffatModel': (lmfit.models.MoffatModel, [['amplitude', 1, int], ['center', 0.0, float], ['sigma', 1, int], ['beta', 1.0, float]]), 'ParabolicModel': (lmfit.models.QuadraticModel, [['a', 0.0, float], ['b', 0.0, float], ['c', 0.0, float]]),
+    'Pearson7Model': (lmfit.models.Pearson7Model, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['expon', 1.0, float]]), 'PolynomialModel': (lmfit.models.PolynomialModel, [['c0', 0, int], ['c1', 0, int], ['c2', 0, int], ['c3', 0, int], ['c4', 0, int], ['c5', 0, int], ['c6', 0, int], ['c7', 0, int], ['degree', 1, int]]),
+    'PowerLawModel': (lmfit.models.PowerLawModel, [['amplitude', 1, int], ['exponent', 1.0, float]]), 'PseudoVoigtModel': (lmfit.models.PseudoVoigtModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0,float], ['fraction', 0.5, float]]),
+    'QuadraticModel': (lmfit.models.QuadraticModel, [['a', 0.0, float], ['b', 0.0, float], ['c', 0.0, float]]), 'RectangleModel': (lmfit.models.RectangleModel, [['amplitude', 1.0, float], ['center1', 0.0, float], ['sigma1', 1.0, float], ['center2', 1.0, float], ['sigma2', 1.0, float], ['form', 'linear', str]]),
+    'SkewedGaussianModel': (lmfit.models.SkewedGaussianModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['gamma', 0.0, float]]), 'SkewedVoigtModel': (lmfit.models.SkewedVoigtModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['gamma', None, type(None)], ['skew', 0.0, float]]),
+    'SplitLorentzianModel': (lmfit.models.SplitLorentzianModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['sigma_r', 1.0, float]]), 'StepModel': (lmfit.models.StepModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['form', 'linear', str]]),
+    'StudentsTModel': (lmfit.models.StudentsTModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float]]), 'ThermalDistributionModel': (lmfit.models.ThermalDistributionModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['kt', 1.0, float], ['form', 'bose', str]]),
+    'VoigtModel': (lmfit.models.VoigtModel, [['amplitude', 1.0, float], ['center', 0.0, float], ['sigma', 1.0, float], ['gamma', None, type(None)]])}
