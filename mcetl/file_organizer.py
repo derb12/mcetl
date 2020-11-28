48291d11151e8a0c353aa6f645a1471b7a9821a5
derb12 <55961724+derb12@users.noreply.github.com>
2020-11-28 13:05:33 -0500

    FIX: Only remove first '.' for file search extension
    Made it so that '.' is removed from the user-input file extension when doing file searching only if the '.' is the first character in the string. This way, file types with multiple extensions, like tar.gz, are now possible to use.
    
    
    Former-commit-id: 062051ddc6ef41f7a32074c7abaec3047cf78a29

--- mcetl/file_organizer.py
+++ mcetl/file_organizer.py
@@ -339,9 +339,13 @@ def _get_keywords(num_keyword_values):
         ])
 
     file_directory = Path(num_keyword_values['folder'])
-    file_type = num_keyword_values['file_type'].replace('.', '')
     min_files = num_keyword_values['min_files']
     max_files = num_keyword_values['max_files']
+    # only take out the preceeding '.', so that multiple suffixes (eg. 'tar.gz') remain intact
+    if num_keyword_values['file_type'].startswith('.'):
+        file_type = num_keyword_values['file_type'][1:]
+    else:
+        file_type = num_keyword_values['file_type']
 
     return file_directory, shared_keywords, unique_keywords, file_type, min_files, max_files
 
