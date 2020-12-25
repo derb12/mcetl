3c45679527e5528924f3ad57405f03bb70d0c67c
derb12 <55961724+derb12@users.noreply.github.com>
2020-12-24 19:23:55 -0500

    OTHER: Update python version in setup.py
    Added python 3.9 to the supported python versions.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Physics',
